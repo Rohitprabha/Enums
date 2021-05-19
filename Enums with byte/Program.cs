1da1acd8f67763782a2c75c02d6cb2d7fac94ea2
Rohitprabha <67499119+Rohitprabha@users.noreply.github.com>
2021-05-19 18:48:49 +0530

    Add files via upload

---
+++ Enums with byte/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enums_with_byte
+{
+    public enum names : byte
+    {
+        Ram = 1,
+        Ashwin = 2,
+        Tej = 3,
+        john = 4
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            names name = names.Ram;
+            byte directionByte;
+            string directionString;
+            Console.WriteLine("name = {0}", name);
+            directionByte = (byte)name;
+            directionString = Convert.ToString(name);
+            Console.WriteLine("byte equivalent = {0}", directionByte);
+            Console.WriteLine("string equivalent = {0}", directionString);
+        }
+    }
+}
